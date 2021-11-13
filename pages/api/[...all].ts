b9210cf2f63b03a06803bb7f4d07e76bf144a4b8
神樹桜乃 <kodama@sakuno.moe>
2021-11-13 21:29:58 +0800

    fix: proxy target

--- pages/api/[...all].ts
+++ pages/api/[...all].ts
@@ -2,10 +2,8 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 import httpProxyMiddleware from 'next-http-proxy-middleware'
 
 const handler = (req: NextApiRequest, res: NextApiResponse) => {
-  req.headers.authorization = `Bearer ${process.env.MTTK_PROXY_ACCESS_TOKEN}`
-
   return httpProxyMiddleware(req, res, {
-    target: 'https://meta-matataki-proxy-q8tdxaj5h-kodamasakuno.vercel.app',
+    target: 'https://meta-governance-api.mttk.net',
     pathRewrite: [
       {
         patternStr: '^/api/',
