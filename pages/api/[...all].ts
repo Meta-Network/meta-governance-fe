a0f809e9302cafc49f1b46a06e4d04bc3d23cc45
神樹桜乃 <kodama@sakuno.moe>
2021-11-13 21:21:36 +0800

    feat: target mttk query api

--- pages/api/[...all].ts
+++ pages/api/[...all].ts
@@ -1,12 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import httpProxyMiddleware from 'next-http-proxy-middleware'
 
-export default (req: NextApiRequest, res: NextApiResponse) => httpProxyMiddleware(req, res, {
-  target: 'http://localhost:3000',
-  pathRewrite: [
-    {
-      patternStr: '^/api/',
-      replaceStr: '/',
-    }
-  ],
-})
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  req.headers.authorization = `Bearer ${process.env.MTTK_PROXY_ACCESS_TOKEN}`
+
+  return httpProxyMiddleware(req, res, {
+    target: 'https://meta-matataki-proxy-q8tdxaj5h-kodamasakuno.vercel.app',
+    pathRewrite: [
+      {
+        patternStr: '^/api/',
+        replaceStr: '/',
+      }
+    ],
+  })
+}
